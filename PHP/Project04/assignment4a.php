b2d4f3ccafe783c275d4a31d7bb583d6d89667d6
dmwalk17 <dmwalk.17@gmail.com>
2017-05-08 15:43:22 -0400

    Php projects added

---
+++ PHP/Project04/assignment4a.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Assignment 4a</title>
+	<!--Name: Donna Walker
+		Class: Web 2603 - WW1
+		Assignment: Project04 - 4A
+		Description: Create an array of specified browsers as part of an 
+		  Object Oriented Program exercise. -->
+	<style>
+	 body{
+		 font-size: 24px;
+         font-family: Sans-serif, Arial;
+         font-weight: bold;
+         color: white;
+	     background-color: #848482;
+	     text-align: center;}	
+     
+	 div{
+		 height: 400px;
+	 }
+	</style>
+  </head>
+  <body>
+	<div></div>
+    <?php
+      //Create function with name greeting to echo Hello, World!
+  $browsers = array('0->Firefox', 'Chrome', 'Internet Explorer', 'Safari', 'Opera',
+                     'Konqurer', 'Other');
+    
+
+    foreach ($browsers as $browser) {
+        echo $browser . ; // displays objects in array
+    } 
+
+    ?>
+	
+    </body>
+  <html>	
+
---
+++ PHP/dateFormats.php
@@ -0,0 +1,16 @@
+<?Php
+//  Donna Walker
+//  Web 2603 WW1
+//  9/26/16
+//  Description: Provides current date in four different formats. 
+//  Required formats are in the echo statements.
+$shortToday = date("m/d/y");
+echo 'Current Date in "MM/DD/YY" format: ' . $shortToday . "<br/>";
+$dayAndWrittenOutToday = date("l, F d, Y");
+echo 'Current Date in "Monday, December 25, 2016" format: '  . $dayAndWrittenOutToday . "<br/>";
+$dateMonthYear = date("d F Y");
+echo 'Current Date in "25 November 2016" format: '  . $dateMonthYear . "<br/>";
+$dateISO = date("c");
+echo 'Current Date in "ISO 8601" format: '  . $dateISO . "<br/>";
+
+?>
