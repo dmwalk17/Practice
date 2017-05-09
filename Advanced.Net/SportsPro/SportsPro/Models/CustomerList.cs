70f73f0fa65b7c864f490cbbcff736169475cea1
dmwalk17 <dmwalk.17@gmail.com>
2017-05-09 16:50:23 -0400

    Beginning of CustomerList Class ~End of Day

---
+++ Advanced.Net/SportsPro/SportsPro/Models/CustomerList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsPro.Models
+{
+    public class CustomerList
+    {
+        private List<Customer> contactList;
+
+        public Customer this[int index] {
+            get
+            {
+                return this.contactList[index];
+            }
+            set
+            {
+                this.contactList[index] = value;
+            }
+        }
+            
+        //public Customer this[string name] { }
+          
+
+        public int Count
+        {
+            get
+            {
+                return contactList.Count();
+            }
+
+        }
+
+        public CustomerList()
+        {
+            CustomerList newList = new CustomerList();
+
+        }
+
+    }
+}
